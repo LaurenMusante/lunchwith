a891944124b883ffef3b96aeb5c9702888de393c
Lauren <Lauren.Musante@gmail.com>
2020-01-30 10:56:06 -0800

    Add styling to header menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import GoogleMap from './components/GoogleMap';
+import Header from './components/Header';
 import data from './data/UserData.json';
 import restaurant from './data/RestaurantData.json';
+
+
 
 function App() {
   return (
     <div>
       <GoogleMap />
+      <Header />
       <p>{data.map(user => user.firstName)}</p>
-      <p>{restaurant.map(restaurant => restaurant.location)}</p>
     </div>
   );
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,16 +3,31 @@
 //hamburger menu that shows a drawer
 
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Icon, Input, Menu } from 'semantic-ui-react';
+import './Header.css'
+
 
 const Header = () => {
   return (
     <div>
-      <Menu>
-        <Menu.Item header>Lunch With</Menu.Item>
-        <Menu.Item name="hi">HI</Menu.Item>
-        <Menu.Item name="hi">HOWDY</Menu.Item>
-        <Menu.Item name="hi">AHOY</Menu.Item>
+      <Menu className="menuStyle">
+        <Menu.Item header>
+          <Icon circular inverted color="teal" name="utensil spoon" />
+          Lunch With
+        </Menu.Item>
+        <Menu.Item>
+          <Input
+            className="icon"
+            icon="search"
+            placeholder="Search Professions..."
+          />
+        </Menu.Item>
+        <Menu.Item name="login">Log In</Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item name="signup">
+            <Icon circular name="bars" />
+          </Menu.Item>
+        </Menu.Menu>
       </Menu>
     </div>
   );
