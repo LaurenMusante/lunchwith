aaef73866fa5fa29dde58701044534b92da61317
Mike McShane <mmcshane10@gmail.com>
2020-02-18 16:28:48 -0800

    add lifecycle component to create array of mentors to map

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -2,54 +2,65 @@
 import React from 'react';
 import Invite from "./Invite"
 import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
+import axios from 'axios';
 require('dotenv').config();
 //wrap map in styled component. 
 
 export class GoogleMap extends React.Component {
-         constructor(props) {
-           super(props);
-           this.state = {
-             stores: [
-               { lat: 45.536428, lng: -122.688748 },
-               { lat: 45.537585, lng: -122.69275 },
-               { lat: 45.532801, lng: -122.694539 }
-             ]
-           };
-         }
+  constructor(props) {
+    super(props);
+    this.state = {
+      mentors: [],
+      stores: [
+        { lat: 45.536428, lng: -122.688748 },
+        { lat: 45.537585, lng: -122.69275 },
+        { lat: 45.532801, lng: -122.694539 }
+      ]
+    };
+  }
 
-         displayMarkers = () => {
-           return this.state.stores.map((store, index) => {
-             return (
-               <Marker
-                 key={index}
-                 id={index}
-                 position={{
-                   lat: store.lat,
-                   lng: store.lng
-                 }}
-                 onClick={() => console.log("HI")}
-               />
-             );
-           });
-         };
-         render() {
-           return (
-             <div>
-               <Map
-                 google={this.props.google}
-                 zoom={14}
-                 style={mapStyle}
-                 initialCenter={{
-                   lat: 45.51223,
-                   lng: -122.658722
-                 }}
-               >
-                 {this.displayMarkers()}
-               </Map>
-             </div>
-           );
-         }
-       }
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/v1/users/')
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+        console.log(persons);
+      })
+  }
+
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: store.lat,
+            lng: store.lng
+          }}
+          onClick={() => console.log("HI")}
+        />
+      );
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyle}
+          initialCenter={{
+            lat: 45.51223,
+            lng: -122.658722
+          }}
+        >
+          {this.displayMarkers()}
+        </Map>
+      </div>
+    );
+  }
+}
 
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLE_API_KEY
